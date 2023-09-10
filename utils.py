a7c96f041078e899ed44e480241def1eb17fd743
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-09-10 21:44:08 +0200

    int

--- utils.py
+++ utils.py
@@ -103,7 +103,7 @@ def test(test_path, model, save_path='./test_result/'):
     img_num = len(img_list) / 2
     suffix = img_list[0].split('.')[-1]
     #print(suffix)
-    img_name_list = list([img_list[i].split('/')[-1].split('.')[0]  for i in range(len(img_list))])
+    img_name_list = list([int(img_list[i].split('/')[-1].split('.')[0])  for i in range(len(img_list))])
     print(img_name_list)
     print(type(img_name_list))
     fusion_phase = test_gray()
