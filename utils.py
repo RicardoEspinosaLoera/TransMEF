57a8e9ee555c9faad6486df2f69c65377d4b6c76
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-09-10 20:44:13 +0200

    img_name_list

--- utils.py
+++ utils.py
@@ -101,6 +101,7 @@ def test(test_path, model, save_path='./test_result/'):
     img_list = glob(test_path + '*')
     img_num = len(img_list) / 2
     suffix = img_list[0].split('.')[-1]
+    print(suffix)
     img_name_list = list(
         set([img_list[i].split('\\')[-1].split('.')[0].strip(string.digits) for i in range(len(img_list))]))
     print(img_name_list)
